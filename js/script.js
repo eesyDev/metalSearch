b65c116abd0ca97b28b5a72ac1eccc960137175d
Rano <khasanova.rano@gmail.com>
2020-09-08 22:21:39 +0600

    popup

--- js/script.js
+++ js/script.js
@@ -1,9 +1,24 @@
-window.addEventListener('DOMContentLoaded', () => {
-    const burgMenu = document.querySelector('.header-wrapper'),
+window.addEventListener('DOMContentLoaded', function() {
+    const slideMenu = document.querySelector('.header-wrapper'),
         burger = document.querySelector('.burger');
 
-        burger.addEventListener('click', () => {
+        burger.addEventListener('click', function() {
             burger.classList.toggle('burger-active');
-            burgMenu.classList.toggle('header-wrapper-active');
-        })
-})
+            slideMenu.classList.toggle('header-wrapper-active');
+        });
+        
+        
+        const popupLink = document.querySelector('.popup-link'),
+              popupOverlay = document.querySelector('.popup'),
+              popupWindow = document.querySelector('.popup-wrapper'),
+              popupClose = document.querySelector('.close');
+
+              popupLink.addEventListener('click', function() {
+                  popupOverlay.classList.toggle('hide');
+                  popupWindow.classList.toggle('hide');
+              });
+              popupClose.addEventListener('click', function() {
+                  popupOverlay.classList.add('hide');
+                  popupWindow.classList.add('hide');
+              })
+});
